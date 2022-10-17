b8e1902570eb73bc41524d1c717836cbafb324c9
lizbethCode <temitopelizzy15@gmail.com>
2022-10-17 12:00:23 -0700

    999

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
-		if (n != 48 || 57)
+		if (n < 57)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	putchar(10);
+	putchar('\n');
 	return (0);
 }
