6cfe9488b030af0618e8da693b8966ca85765f84
lizbethCode <temitopelizzy15@gmail.com>
2022-10-17 12:27:14 -0700

    1004u

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+			if (x != 8 || (x == 8 && y != 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
