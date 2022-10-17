629b942a733988ab4427b6b4bd6d096af2e6baf4
lizbethCode <temitopelizzy15@gmail.com>
2022-10-17 10:29:08 -0700

    alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Retun: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 97; n < 123; n++)
+	{
+		if (n != 'q' && n != 'e')
+			putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
