1da10f8cd94b8553abaf0aa736e63407b37b1347
lizbethCode <temitopelizzy15@gmail.com>
2022-10-17 11:22:43 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 122; n > 96; n--)
+		putchar(n);
+	putchar(10);
+	return (0);
+}
