496ae10185371fab21ef02f6d71e05c94b828683
lizbethCode <temitopelizzy15@gmail.com>
2022-11-16 18:07:30 -0800

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,28 +1,18 @@
+#include <stdlib.h>
 #include "main.h"
+
 /**
- * *cap_string - capitalizes all words of a string
- * @s: string
+ * _abs - absolute value of an integer.
+ * @i: absolute integer
+ *
  * Return: Always 0
  */
-char *cap_string(char *s)
+int _abs(int i)
 {
-	int i;
+	if (i > 0)
+		return (i);
+	else if (i < 0)
+		return (-i);
 
-	if (s[0] >= 'a' && s[0] <= 'z')
-	{
-		s[0] -= 32;
-	}
-	for (i = 1; s[i]; i++)
-	{
-		if ((s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t'
-		    || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '!'
-		    || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '('
-		     || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}'
-		     || s[i - 1] == '.') && (s[i] > 'a' && s[i] < 'z'))
-		{
-			s[i] -= 32;
-		}
-	}
-
-	return (s);
+	return (0);
 }
