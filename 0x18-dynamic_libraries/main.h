bb4b477a4cbb47dfa3aee5c66f2c404e2350d435
lizbethcode <temitopelizzy15@gmail.com>
2023-01-24 15:48:49 -0800

    Help

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef FILE_H
-#define FILE_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,7 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-void _puts(char *str)
 
 #endif
